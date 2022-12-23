012fcfb1b7df527ddf479b773ba7453e6982061b
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-23 16:06:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let cats =["Milo", "Otis", "Garfield"]
+  
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+     let newcat = [...cats.slice(0),name]
+     return newcat
+}
+function prependCat(name){
+    let newcat = [name, ...cats.slice(0)]
+    return newcat
+}
+function removeLastCat(){
+    let newcat = cats.slice(0,cats.length-1)
+    return newcat
+}
+function removeFirstCat(){
+    let newcat = cats.slice(1)
+    return newcat
+}
